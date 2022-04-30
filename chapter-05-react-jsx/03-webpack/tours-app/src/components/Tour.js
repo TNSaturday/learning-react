0d5c677e0a0cfbfc813f45681530bbfe1889b2de
TNsaturday <tnsaturday@gmail.com>
2022-04-30 14:17:31 +0300

    Finish StarRating component

--- chapter-05-react-jsx/03-webpack/tours-app/src/components/Tour.js
+++ chapter-05-react-jsx/03-webpack/tours-app/src/components/Tour.js
@@ -1,14 +1,17 @@
 import React from "react";
+
 import Header from "./Header";
 import Sights from "./Sights";
 import Price from "./Price";
+import StarRating from "./StarRating";
 
-export default function Tour ({ destination, sights, price }) {
+export default function Tour ({ destination, sights, price, rating }) {
     return (
         <>
             <Header destination={destination} />
             <Sights sights={sights}/>
             <Price price={price}/>
+            <StarRating totalStars={5} rating={rating} />
         </>
     )
 }
