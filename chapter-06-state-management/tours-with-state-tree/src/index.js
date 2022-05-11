25c36670ae7863009c5d207daab69f8a3391d617
TNsaturday <tnsaturday@gmail.com>
2022-05-11 17:03:10 +0300

    Remove unused code

--- chapter-06-state-management/tours-with-state-tree/src/index.js
+++ chapter-06-state-management/tours-with-state-tree/src/index.js
@@ -1,10 +1,8 @@
-import React, { createContext } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TourProvider from "./providers/TourProvider";
 
 import App from './App';
-
-export const TourContext = createContext(undefined);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
